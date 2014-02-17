c3b5abb3cd45c9d98d7874894602bfb7744238c2
Richard Bennett <rmb209@ic.ac.uk>
2014-02-17 10:56:01 +0000

    Optimised on DoC PCs 7.67 - 6.96

--- src/rmb209/fast_fourier_transform_opt.cpp
+++ src/rmb209/fast_fourier_transform_opt.cpp
@@ -1,11 +1,11 @@
 #include "fourier_transform.hpp"
-#include <thread>
+
 #include <cmath>
 #include <cassert>
 #include <tbb/tbb.h>
 #include <tbb/parallel_for.h>
 #include "tbb/task_group.h"
-
+#include <thread>
 namespace hpce
 {
 namespace rmb209
@@ -35,16 +35,12 @@ protected:
 	{
 		assert(n>0);
 		
-		if (n == 1)
-		{
+		if (n == 1){
 			pOut[0] = pIn[0];
-		}
-		else if (n == 2)
-		{
+		}else if (n == 2){
 			pOut[0] = pIn[0]+pIn[sIn];
 			pOut[sOut] = pIn[0]-pIn[sIn];
-		}else
-		{
+		}else{
 			size_t m = n/2;
 
 			/*Old Implementation
@@ -52,20 +48,27 @@ protected:
 			//forwards_impl(m,wn*wn,pIn+sIn,2*sIn,pOut+sOut*m,sOut);
 			*/
 			//Task Group Activities
-			if(n > 5)
-			{
-				tbb::task_group group;
-				group.run( [&](){forwards_impl(m,wn*wn,pIn,2*sIn,pOut,sOut);}); 
-				group.run( [&](){forwards_impl(m,wn*wn,pIn+sIn,2*sIn,pOut+sOut*m,sOut);});
-				group.wait();
-			}
-			else
-			{
-				forwards_impl(m,wn*wn,pIn,2*sIn,pOut,sOut);
-				forwards_impl(m,wn*wn,pIn+sIn,2*sIn,pOut+sOut*m,sOut);
-			} 
-			//size_t K = 128;
-			size_t K = std::thread::hardware_concurrency()*2;
+
+			//tbb::task_group group;
+			//group.run( [&](){forwards_impl(m,wn*wn,pIn,2*sIn,pOut,sOut);}); 
+			//group.run( [&](){forwards_impl(m,wn*wn,pIn+sIn,2*sIn,pOut+sOut*m,sOut);});
+			//group.wait();
+
+			if(n > (std::thread::hardware_concurrency()+1))
+                        {
+                                tbb::task_group group;
+                                group.run( [&](){forwards_impl(m,wn*wn,pIn,2*sIn,pOut,sOut);});
+                                group.run( [&](){forwards_impl(m,wn*wn,pIn+sIn,2*sIn,pOut+sOut*m,sOut);});
+                                group.wait();
+                        }
+                        else
+                        {
+                                forwards_impl(m,wn*wn,pIn,2*sIn,pOut,sOut);
+                                forwards_impl(m,wn*wn,pIn+sIn,2*sIn,pOut+sOut*m,sOut);
+                        }
+
+			//size_t K = std::thread::hardware_concurrency();			 
+			size_t K = 8;
 			if(m > K)
 			{
 				tbb::parallel_for(size_t(0), (m/K), [=](size_t j0)
@@ -87,15 +90,15 @@ protected:
 			else
 			{
 				std::complex<double> w=std::complex<double>(1.0, 0.0);
-				for (size_t j=0;j<m;j++)
-				{
+			for (size_t j=0;j<m;j++)
+			{
 
-			  		std::complex<double> t1 = w*pOut[m+j];
-			  		std::complex<double> t2 = pOut[j]-t1;
-			  		pOut[j] = pOut[j]+t1;                 //  pOut[j] = pOut[j] + w^i pOut[m+j] 
-			  		pOut[j+m] = t2;                          //  pOut[j] = pOut[j] - w^i pOut[m+j] 
-			  		w = w*wn;
-				}
+			  std::complex<double> t1 = w*pOut[m+j];
+			  std::complex<double> t2 = pOut[j]-t1;
+			  pOut[j] = pOut[j]+t1;                 //  pOut[j] = pOut[j] + w^i pOut[m+j] 
+			  pOut[j+m] = t2;                          //  pOut[j] = pOut[j] - w^i pOut[m+j] 
+			  w = w*wn;
+			}
 
 			}
 		}
@@ -111,8 +114,7 @@ protected:
 		forwards_impl(n, reverse_wn, pIn, sIn, pOut, sOut);
 		
 		double scale=1.0/n;
-		for(size_t i=0;i<n;i++)
-		{
+		for(size_t i=0;i<n;i++){
 			pOut[i]=pOut[i]*scale;
 		}
 	}
